a3a99f7e574d63ffd1916177333f85fcb97b1b10
unknown <IHOMS@IHOMS.vsmmc.local>
2024-02-15 10:03:56 +0800

    design signin

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +11,30 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        title : 'Login'
+      }
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const pageTitle = to.meta.title;
+  if (pageTitle) {
+    document.title = pageTitle;
+  }
+  next();
+});
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,69 @@
+<template lang="">
+    <div class="red sign-in-container d-flex align-center justify-start">
+        <div class="blue login-box d-flex flex-column justify-center align-center">
+            <div class="red w-100 d-flex flex-column align-center pa-5">
+                <h2>Lorem, ipsum dolor. {{ name }}</h2>
+                <div class="input-field w-100">
+                    <v-form>
+                        <v-text-field
+                           v-model="username"
+                           name="username"
+                           label="Username"
+                           id="id"
+                           variant="outlined"
+                           clearable
+                        ></v-text-field>
+                        <v-text-field
+                        v-model="password"
+                           name="password"
+                           label="Password"
+                           id="id"
+                           :type="passInputType"
+                           variant="outlined"
+                           hint="Please remember your password"
+                           :append-inner-icon="showPassword ? 'mdi-eye-off-outline' : 'mdi-eye'"
+                           @click:append-inner="togglePasswordVisibilty()"
+                        ></v-text-field>
+                        <v-btn @click="printUser()" size="large" color="secondary w-100 mt-2">Sign in</v-btn>
+                    </v-form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref, computed } from "vue";
+
+const name = ref("gaby");
+const username = ref("");
+
+
+const password = ref("");
+const showPassword = ref(false)
+
+const passInputType = computed(() => (showPassword.value ? 'text' : 'password'))
+
+const printUser = () => {
+  console.log(username.value);
+  console.log(password.value);
+};
+
+const togglePasswordVisibilty = () => {
+   showPassword.value = !showPassword.value
+}
+
+</script>
+<style lang="css">
+.sign-in-container {
+  height: 100vh;
+  width: 100%;
+  padding: 1.5em;
+}
+.login-box {
+  padding: 1em;
+  height: 95%;
+  width: 30%;
+  border-radius: 15px;
+  /* border: 1px solid blue; */
+}
+</style>
