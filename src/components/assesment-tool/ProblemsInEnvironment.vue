8b69888d2dace6f7efad60e75fe6256f1343c87b
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-03-15 10:04:23 +0800

    created ui for ix

--- src/components/assesment-tool/ProblemsInEnvironment.vue
+++ src/components/assesment-tool/ProblemsInEnvironment.vue
@@ -219,9 +219,59 @@
             </tbody>
           </v-table>
         </v-container>
-        {{ problemsInEnvironment}}
+        {{ problemsInEnvironment }}
       </v-window-item>
-      <v-window-item :value="2">2</v-window-item>
+      <v-window-item :value="2">
+        <v-container style="width: 1500px">
+          <h1>Problems in the Environment</h1>
+          <v-divider class="bm-5"></v-divider>
+          <v-row>
+            <v-col
+              cols="12"
+              v-for="(field, key) in inputFields.comboBox"
+              :key="key"
+            >
+              <v-combobox
+                chips
+                multiple
+                clearable
+                closable-chips
+                variant="outlined"
+                :label="field.label"
+                :items="field.items"
+                v-model="problemsInEnvironment[key]"
+              ></v-combobox>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="4"
+              v-for="(field, key) in inputFields.textArea"
+              :key="key"
+            >
+              <v-textarea
+                variant="outlined"
+                :label="field.label"
+                v-model="problemsInEnvironment[key]"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="4"
+              v-for="(field, key) in inputFields.textField"
+              :key="key"
+            >
+              <v-text-field
+                variant="outlined"
+                :label="field.label"
+                v-model="problemsInEnvironment[key]"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        {{ problemsInEnvironment }}
+      </v-window-item>
     </v-window>
   </div>
 </template>
@@ -308,7 +358,6 @@ const snackBars = ref({
 const handleSnackBar = (type) => {
   snackBars.value[type].isActive = true;
 };
-
 const tab = ref(0);
 const tabHeaders = [
   { title: "slide1", value: 1 },
@@ -372,7 +421,6 @@ const particulars = {
     },
   },
 };
-
 const indexItems = {
   severity: [
     { text: "No Problem", value: 1 },
@@ -391,7 +439,81 @@ const indexItems = {
     { text: "less than 2 weeks", value: 6 },
   ],
 };
-
+const inputFields = {
+  comboBox: {
+    problems_presented: {
+      label: "Problems Presented At Intake",
+      items: [
+        "Housing",
+        "Medical Management",
+        "Food/Nutrition",
+        "Lack of knowledge/information about where to get medical assistance",
+        "Employment",
+        "Lack of knowledge/information about hospital protocol",
+        "Economic Resources",
+        "No immediate family members to assist the patient",
+        "Health Condition of the Patient",
+      ],
+    },
+    reasons_psychosocial_counselling: {
+      label: "Reasons for Psychosocial Counselling",
+      items: [
+        "Anxiety of feelings of helplessness about the treatment expenses",
+        "Large family as reason for not being able to provide for their economic needs",
+        "Anxiety over illness and treatment",
+        "Complacency and dependency for assistance",
+        "Unsure and unprepared for the responsibilities of parenting including young parents’ issues",
+        "Environment/role adjustment problem",
+        "Confusion about the delivery system",
+        "Unfounded fear of death, disfigurement",
+        "Stress of patient’s relatives",
+        "Emotional disturbance/imbalance",
+        "Aggressiveness/hostility/stubbornness and demanding traits",
+        "Inadequate/absence of affectional support system",
+        "Inadequate/absence of coping skills for problem solving and decision making",
+        "Marital and family relationship issues",
+        "Worried about the loss of income because of illness",
+        "Responsibility type of social interaction problem",
+        "Refusal to follow rules and hospital policies",
+        "Lack of confidence about support of partner",
+        "Poor compliance to the doctors ‘instructions",
+        "Victim of abuse",
+        "Going home against medical advice",
+        "Advocacy for patient’s rights on work-related and medico-legal issues",
+        "Suicide and self-inflected injuries",
+        "Alcohol, cigarette and other substance abuse",
+      ],
+    },
+  },
+  textArea: {
+    assesment_findings: {
+      label: "Assesment Findings",
+    },
+    recommended_intervention: {
+      label: "Recommended Intervention",
+    },
+    action_taken: {
+      label: "Action Taken",
+    },
+  },
+  textField: {
+    person_emergency: {
+      label: "Person to be notified in case of emergency",
+    },
+    relationship_to_patient: {
+      label: "Relation to the patient",
+    },
+    address: {
+      label: "Address",
+    },
+    contact_number: {
+      label: "Contact Number",
+    },
+    interviewed_by: {
+      label: "Interviewer",
+    },
+  },
+};
 const handleButtonAction = async () => {};
 const handlePatientData = (response) => {
   problemsInEnvironment.value = response;
