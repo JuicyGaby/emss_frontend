35af271df668debbf6faf429a3133b4ebb0c8376
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-03-22 15:05:29 +0800

    imported and created swa api's

--- src/api/daily-activity-report.js
+++ src/api/daily-activity-report.js
@@ -34,9 +34,51 @@ const updateDailyActivityReport = async (body) => {
   const data = await response.json();
   return data;
 };
+
+// SWA
+const createSocialWorkAdministration = async (body) => {
+  const API_URL = `http://localhost:3000/social-work-administration`;
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+};
+const getSocialWorkAdministration = async () => {
+  const API_URL = `http://localhost:3000/social-work-administration`;
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
+const getSocialWorkAdministrationById = async (id) => {
+  const API_URL = `http://localhost:3000/social-work-administration/${id}`;
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
+const updateSocialWorkAdministration = async (body) => {
+  const API_URL = `http://localhost:3000/social-work-administration`;
+  const response = await fetch(API_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+};
 export {
   createDailyActivityReport,
   getDailyActivityReport,
   getDailyActivityReportById,
   updateDailyActivityReport,
+  createSocialWorkAdministration,
+  getSocialWorkAdministration,
+  getSocialWorkAdministrationById,
+  updateSocialWorkAdministration,
 };
