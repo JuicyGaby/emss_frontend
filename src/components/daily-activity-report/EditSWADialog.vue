c3c64a991d291806595f896e50d924d060a2e7f6
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-03-25 11:50:36 +0800

    updated inputfields value

--- src/components/daily-activity-report/EditSWADialog.vue
+++ src/components/daily-activity-report/EditSWADialog.vue
@@ -102,64 +102,78 @@
             </div>
             <h2>IV. Human Resource Development</h2>
             <div class="px-5 mt-5">
-                <v-select
-                    multiple
-                    chips
-                    closable-chips
-                    :label="inputFields.humanResourceDevelopment.humanResourceDevelopment.label"
-                    :items="inputFields.humanResourceDevelopment.humanResourceDevelopment.items"
-                    item-title="text"
-                    item-value="value"
-                    variant="outlined"
-                    density="comfortable"
-                ></v-select>
-                <v-select
-                    multiple
-                    chips
-                    closable-chips
-                    :label="inputFields.humanResourceDevelopment.affiliationProgram.label"
-                    :items="inputFields.humanResourceDevelopment.affiliationProgram.items"
-                    item-title="text"
-                    item-value="value"
-                    variant="outlined"
-                    density="comfortable"
-                ></v-select>
-                <v-select
-                    multiple
-                    chips
-                    closable-chips
-                    :label="inputFields.humanResourceDevelopment.staffDevelopment.label"
-                    :items="inputFields.humanResourceDevelopment.staffDevelopment.items"
-                    item-title="text"
-                    item-value="value"
-                    variant="outlined"
-                    density="comfortable"
-                ></v-select>
+              <v-select
+                multiple
+                chips
+                closable-chips
+                :label="
+                  inputFields.humanResourceDevelopment.humanResourceDevelopment
+                    .label
+                "
+                :items="
+                  inputFields.humanResourceDevelopment.humanResourceDevelopment
+                    .items
+                "
+                item-title="text"
+                item-value="value"
+                variant="outlined"
+                density="comfortable"
+              ></v-select>
+              <v-select
+                multiple
+                chips
+                closable-chips
+                :label="
+                  inputFields.humanResourceDevelopment.affiliationProgram.label
+                "
+                :items="
+                  inputFields.humanResourceDevelopment.affiliationProgram.items
+                "
+                item-title="text"
+                item-value="value"
+                variant="outlined"
+                density="comfortable"
+              ></v-select>
+              <v-select
+                multiple
+                chips
+                closable-chips
+                :label="
+                  inputFields.humanResourceDevelopment.staffDevelopment.label
+                "
+                :items="
+                  inputFields.humanResourceDevelopment.staffDevelopment.items
+                "
+                item-title="text"
+                item-value="value"
+                variant="outlined"
+                density="comfortable"
+              ></v-select>
             </div>
             <h2>V. Social Work Research</h2>
             <div class="px-5 mt-5">
-                <v-select
-                    multiple
-                    chips
-                    closable-chips
-                    :label="inputFields.socialWorkResearch.socialWorkResearch.label"
-                    :items="inputFields.socialWorkResearch.socialWorkResearch.items"
-                    item-title="text"
-                    item-value="value"
-                    variant="outlined"
-                    density="comfortable"
-                ></v-select>
-                <v-select
-                    multiple
-                    chips
-                    closable-chips
-                    :label="inputFields.socialWorkResearch.reporting.label"
-                    :items="inputFields.socialWorkResearch.reporting.items"
-                    item-title="text"
-                    item-value="value"
-                    variant="outlined"
-                    density="comfortable"
-                ></v-select>
+              <v-select
+                multiple
+                chips
+                closable-chips
+                :label="inputFields.socialWorkResearch.socialWorkResearch.label"
+                :items="inputFields.socialWorkResearch.socialWorkResearch.items"
+                item-title="text"
+                item-value="value"
+                variant="outlined"
+                density="comfortable"
+              ></v-select>
+              <v-select
+                multiple
+                chips
+                closable-chips
+                :label="inputFields.socialWorkResearch.reporting.label"
+                :items="inputFields.socialWorkResearch.reporting.items"
+                item-title="text"
+                item-value="value"
+                variant="outlined"
+                density="comfortable"
+              ></v-select>
             </div>
             <v-btn color="success">Update</v-btn>
           </v-col>
@@ -174,75 +188,75 @@ const inputFields = {
   documentation: {
     label: "Documentation",
     items: [
-      { text: "MSWD Assessment Tool", value: "" },
-      { text: "Social Profile with Social Care Plan", value: "" },
-      { text: "Medical Social Worker's Progress Note", value: "" },
-      { text: "Group Work Recording", value: "" },
-      { text: "Social Case Study Report", value: "" },
-      { text: "Social Case Summary", value: "" },
-      { text: "Home Visit Report", value: "" },
-      { text: "Service Card", value: "" },
-      { text: "Registration Book", value: "" },
-      { text: "Daily Activity Report", value: "" },
-      { text: "Feedback Report", value: "" },
+      { text: "MSWD Assessment Tool", value: 1 },
+      { text: "Social Profile with Social Care Plan", value: 2 },
+      { text: "Medical Social Worker's Progress Note", value: 3 },
+      { text: "Group Work Recording", value: 4 },
+      { text: "Social Case Study Report", value: 5 },
+      { text: "Social Case Summary", value: 6 },
+      { text: "Home Visit Report", value: 7 },
+      { text: "Service Card", value: 8 },
+      { text: "Registration Book", value: 9 },
+      { text: "Daily Activity Report", value: 10 },
+      { text: "Feedback Report", value: 11 },
     ],
   },
   programManagement: {
     programManagement: {
       label: "Program Management",
       items: [
-        { text: "MSWD Fund Raising Activity", value: "" },
-        { text: "Hospital Activity", value: "" },
-        { text: "Networking (Meeting with other institutions)", value: "" },
-        { text: "Coordination with Mass Media", value: "" },
-        { text: "Utilization Review", value: "" },
-        { text: "Policy Development", value: "" },
-        { text: "Planning and Budgeting", value: "" },
-        { text: "Advocacy for MSWD Office (Head and Supervisors)", value: "" },
+        { text: "MSWD Fund Raising Activity", value: 14 },
+        { text: "Hospital Activity", value: 16 },
+        { text: "Networking (Meeting with other institutions)", value: 17 },
+        { text: "Coordination with Mass Media", value: 18 },
+        { text: "Utilization Review", value: 20 },
+        { text: "Policy Development", value: 21 },
+        { text: "Planning and Budgeting", value: 22 },
+        { text: "Advocacy for MSWD Office (Head and Supervisors)", value: 23 },
       ],
     },
     committeeMeetings: {
       label: "Committee Meetings",
       items: [
-        { text: "Attendance to Clinical Committees", value: "" },
-        { text: "Attendance to Clinical Committees", value: "" },
+        { text: "Attendance to Clinical Committees", value: 12 },
+        { text: "Attendance to Clinical Committees", value: 13 },
       ],
     },
     donationsManagementProgram: {
       label: "Donations Management Program",
-      items: [{ text: "Linkaged with Donors", value: "" }],
+      items: [{ text: "Linkaged with Donors", value: 15 }],
     },
     consultativeAdvisoryServices: {
       label: "Consultative Advisory Services",
-      items: [{ text: "Co-Employees", value: "" }],
+      items: [{ text: "Co-Employees", value: 19 }],
     },
   },
   Supervision: {
     supervision1: {
       label: "Supervision",
       items: [
-        { text: "Monitoring (Head and Supervisors)", value: "" },
-        { text: "Checking Daily Plans and Daily Activity Report", value: "" },
-        { text: "Checking of Recording", value: "" },
-        { text: "Random checking of Charts", value: "" },
+        { text: "Monitoring (Head and Supervisors)", value: 24 },
+        { text: "Checking Daily Plans and Daily Activity Report", value: 27 },
+        { text: "Checking of Recording", value: 28 },
+        { text: "Random checking of Charts", value: 29 },
       ],
     },
     supervision2: {
       label: "Supervision (Supervisor and Senior Staff)",
       items: [
-        { text: "Clinical Supervision", value: "" },
-        { text: "Administrative Supervision", value: "" },
+        { text: "Clinical Supervision", value: 25 },
+        { text: "Administrative Supervision", value: 26 },
       ],
     },
     staffMeeting: {
       label: "Staff Meeting",
       items: [
-        { text: "Execom/Mancom", value: "" },
-        { text: "General", value: "" },
-        { text: "Team", value: "" },
-        { text: "Coaching", value: "" },
-        { text: "Mentoring", value: "" },
-        { text: "Staff Case Conference", value: "" },
+        { text: "Execom/Mancom", value: 30 },
+        { text: "General", value: 31 },
+        { text: "Team", value: 32 },
+        { text: "Coaching", value: 33 },
+        { text: "Mentoring", value: 34 },
+        { text: "Staff Case Conference", value: 35 },
       ],
     },
   },
@@ -250,47 +264,47 @@ const inputFields = {
     humanResourceDevelopment: {
       label: "Human Resource Development",
       items: [
-        { text: "Orientation and Interpretation of MSWD Services", value: "" },
-        { text: "Staff Recruitment and Selection", value: "" },
+        { text: "Orientation and Interpretation of MSWD Services", value: 43 },
+        { text: "Staff Recruitment and Selection", value: 44 },
       ],
     },
     affiliationProgram: {
       label: "Affiliation Program",
       items: [
-        { text: "Orientation", value: "" },
-        { text: "Trialogue", value: "" },
-        { text: "Supervision", value: "" },
+        { text: "Orientation", value: 36 },
+        { text: "Trialogue", value: 37 },
+        { text: "Supervision", value: 38 },
       ],
     },
     staffDevelopment: {
       label: "Staff Development",
       items: [
-        { text: "Attended LDI outside the health facilities", value: "" },
-        { text: "Conduct LDI Needs Assessment", value: "" },
-        { text: "Development of LDI Design", value: "" },
-        { text: "Self Directed Learnings", value: "" },
+        { text: "Attended LDI outside the health facilities", value: 39 },
+        { text: "Conduct LDI Needs Assessment", value: 40 },
+        { text: "Development of LDI Design", value: 41 },
+        { text: "Self Directed Learnings", value: 42 },
       ],
     },
   },
   socialWorkResearch: {
     socialWorkResearch: {
       label: "Social Work Research",
-      items: [{ text: "Monitoring and Evaluation", value: "" }],
+      items: [{ text: "Monitoring and Evaluation", value: 53 }],
     },
     reporting: {
       label: "Reporting",
       items: [
-        { text: "Statistical and Narrative", value: "" },
-        { text: "Annual Psycho-Social Profile of Patients Served", value: "" },
-        { text: "VAWC Statistical Report", value: "" },
-        { text: "HIV Report", value: "" },
-        { text: "Malasakit Center Statistical Report", value: "" },
+        { text: "Statistical and Narrative", value: 45 },
+        { text: "Annual Psycho-Social Profile of Patients Served", value: 46 },
+        { text: "VAWC Statistical Report", value: 47 },
+        { text: "HIV Report", value: 48 },
+        { text: "Malasakit Center Statistical Report", value: 49 },
         {
           text: "Endowment Fund Utilization Report/Other Fund Reports",
-          value: "",
+          value: 50,
         },
-        { text: "BJMP Monitoring Report", value: "" },
-        { text: "OWWA Report", value: "" },
+        { text: "BJMP Monitoring Report", value: 51 },
+        { text: "OWWA Report", value: 52 },
       ],
     },
   },
