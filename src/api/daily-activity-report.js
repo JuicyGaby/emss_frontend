fb1ad65acd75e34915f55b39924590e7d916ef77
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-04-01 09:29:43 +0800

    integrated post get put delete api for dar note

--- src/api/daily-activity-report.js
+++ src/api/daily-activity-report.js
@@ -47,6 +47,52 @@ const updateDailyActivityReport = async (body) => {
   const data = await response.json();
   return data;
 };
+//  DAR NOTES
+
+const createDarNotes = async (body) => {
+  const API_URL = `http://localhost:3000/dar-notes`;
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+};
+const getDarNotes = async () => {
+  const API_URL = `http://localhost:3000/dar-notes`;
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
+const getDarNoteById = async (id) => {
+  const API_URL = `http://localhost:3000/dar-notes/${id}`;
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
+const updateDarNote = async (body) => {
+  const API_URL = `http://localhost:3000/dar-notes`;
+  const response = await fetch(API_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+};
+const deleteDarNote = async (id) => {
+  const API_URL = `http://localhost:3000/dar-notes/${id}`;
+  const response = await fetch(API_URL, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  return data;
+};
 
 // SWA
 const createSocialWorkAdministration = async (body) => {
@@ -85,13 +131,13 @@ const updateSocialWorkAdministration = async (body) => {
   const data = await response.json();
   return data;
 };
-
 const getDarServices = async () => {
   const API_URL = `http://localhost:3000/dar-services`;
   const response = await fetch(API_URL);
   const data = await response.json();
   return data;
 };
+
 export {
   createDailyActivityReport,
   getDailyActivityReport,
