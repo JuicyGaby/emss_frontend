fde89f59e89dc1c6a6ac2607c62dfb4cd4ff46db
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-04-29 07:55:21 +0800

    updated indexed view and added social worker dashboard

---
+++ src/components/dashboard/SocialWorkerDashboard.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div>
+    this is social worker
+  </div>
+</template>
+<script setup></script>
+<style lang=""></style>
--- src/views/indexView.vue
+++ src/views/indexView.vue
@@ -6,9 +6,56 @@
         <h1>Electronic Malasakit Social Service System</h1>
       </v-toolbar>
       <v-card-text>
-        <h1 class="text-h4">
-          {{ currentDate }} - {{ moment().format("dddd") }}
-        </h1>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <h1 class="text-h4">
+                {{ currentDate }} - {{ moment().format("dddd") }}
+              </h1>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-select
+                v-model="userInputs.month"
+                :items="inputFields.month.items"
+                :label="inputFields.month.label"
+                variant="outlined"
+                density="comfortable"
+                style="width: 500px"
+                class="my-1"
+              ></v-select>
+            </v-col>
+            <v-col cols="4" v-for="(item, index) in userCards" :key="index">
+              <v-card height="auto">
+                <v-toolbar rounded :color="item.toolBarColor" class="px-5">
+                  <v-icon size="x-large" class="mr-4">{{ item.icon }}</v-icon>
+                  <h2>{{ item.label }}</h2>
+                </v-toolbar>
+                <v-card-text class="d-flex justify-center align-center">
+                  <h1 class="text-h3">
+                    {{ item.count }}
+                  </h1>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="4" v-for="(item, index) in cards" :key="index">
+              <v-card height="auto">
+                <v-toolbar rounded :color="item.toolBarColor" class="px-5">
+                  <v-icon size="x-large" class="mr-4">{{ item.icon }}</v-icon>
+                  <h2>{{ item.title }}</h2>
+                </v-toolbar>
+                <v-card-text class="d-flex justify-center align-center">
+                  <h1 class="text-h3">
+                    {{ item.count }}
+                  </h1>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card-text>
     </v-card>
   </div>
@@ -16,11 +63,78 @@
 <script setup>
 import moment from "moment";
 import { userAuthentication } from "@/stores/session";
-import { validateForm } from "@/utils/constants";
-import { getDarByMonth } from "@/api/daily-activity-report";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
+import { handleSnackBar } from "@/utils/constants";
+// components
+import snackBars from "@/components/dialogs/snackBars.vue";
+import SocialWorkerDashboardVue from "@/components/dashboard/SocialWorkerDashboard.vue";
 const authentication = userAuthentication();
 const userFullName = `${authentication.user.fname} ${authentication.user.lname}`;
 const currentDate = moment().format("MMMM Do YYYY");
+const currentMonth = moment().format("MMMM");
+const snackBarData = ref({});
+const userInputs = ref({
+  month: currentMonth,
+});
+const cards = ref({
+  monthlyDarCount: {
+    title: "Monthly DAR Count",
+    icon: "mdi-notebook-check",
+    toolBarColor: "green-lighten-1",
+    count: 0,
+  },
+  monthlySwaCount: {
+    title: "Monthly SWA Count",
+    icon: "mdi-notebook-check-outline",
+    toolBarColor: "orange-lighten-1",
+    count: 0,
+  },
+  monthlyAssesedPatientsCount: {
+    title: "Monthly Assesed Patients Count",
+    icon: "mdi-account-multiple",
+    toolBarColor: "blue-lighten-1",
+    count: 0,
+  },
+});
+const userCards = ref({
+  dar: {
+    label: "My DAR Created Count",
+    icon: "mdi-notebook-check",
+    toolBarColor: "success",
+    count: 0,
+  },
+  swa: {
+    label: "My SWA Created Count",
+    icon: "mdi-notebook-check-outline",
+    toolBarColor: "warning",
+
+    count: 0,
+  },
+  assesedPatients: {
+    label: "My Assesed Patients Count",
+    icon: "mdi-account-multiple",
+    toolBarColor: "primary",
+    count: 0,
+  },
+});
+const inputFields = ref({
+  month: {
+    label: "Selected Month",
+    items: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+  },
+});
 </script>
 <style lang=""></style>
