73f33f7b5ac09942fb6e375781bec28931e2d541
Gabe Fletcher Sanchez <gabefletchers@gmail.com>
2024-05-14 13:31:52 +0800

    refactor: Update PatientsView.vue for improved search functionality and user experience

--- src/views/PatientsView.vue
+++ src/views/PatientsView.vue
@@ -6,36 +6,33 @@
         <h1 class="">Assessment Tool</h1>
       </v-toolbar>
       <div class="ma-3 d-flex justify-space-between align-center">
-        <div class="">
+        <v-btn
+          color="secondary"
+          prepend-icon="mdi-account-plus"
+          @click="createDialog = !createDialog"
+          >Assess Patient</v-btn
+        >
+        <div class="d-flex ga-2">
+          <v-text-field
+            v-for="(item, key) in inputFields.search"
+            :key="key"
+            :label="item.label"
+            v-model="searchInput[key]"
+            density="compact"
+            style="width: 200px"
+            variant="outlined"
+            @keyup.enter="searchPatientData"
+          ></v-text-field>
           <v-btn
-            color="secondary"
-            prepend-icon="mdi-account-plus"
-            @click="createDialog = !createDialog"
-            >Assess Patient</v-btn
+            prepend-icon="mdi-account-search"
+            color="grey"
+            @click="searchPatientData"
+            :disabled="!searchInput.first_name && !searchInput.last_name"
           >
+            Search Patient
+          </v-btn>
+          <!-- {{ searchInput }} -->
         </div>
-        <v-form ref="searchForm">
-          <div class="d-flex ga-2">
-            <v-text-field
-              v-for="(item, key) in inputFields.search"
-              :key="key"
-              :label="item.label"
-              v-model="searchInput[key]"
-              density="compact"
-              style="width: 200px"
-              variant="outlined"
-              @keyup.enter="searchPatientData"
-            ></v-text-field>
-            <v-btn
-              color="grey"
-              @click="searchPatientData"
-              :disabled="!searchInput.first_name && !searchInput.last_name"
-            >
-              Search
-            </v-btn>
-            <!-- {{ searchInput }} -->
-          </div>
-        </v-form>
       </div>
       <v-divider></v-divider>
       <v-card-text class="d-flex align-end">
@@ -281,9 +278,6 @@ async function fetchPatients() {
   }
 }
 async function searchPatientData() {
-  const isValid = await validateForm(searchForm);
-  if (!isValid) return;
-
   const patients = await searchPatient(searchInput.value);
   if (patients.length <= 0) {
     snackBarData.value = handleSnackBar("error", "No data found");
